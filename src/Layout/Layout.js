0d3a89ae41227c72edce0178810949d19c2f20f2
Lara Balsamello <lara.balsamello@gmail.com>
2020-02-15 12:18:00 -0300

    basic layout works

---
+++ src/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Auxiliary from "../hoc/Auxiliary";
+
+
+class Layout extends Component {
+
+    render() {
+        return (
+            <Auxiliary>
+                <div>TEST</div>
+            </Auxiliary>)
+    }
+}
+
+export default Layout;
