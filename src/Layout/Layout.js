838fc0b7729bb6f03113fb9badb3f34e39718d97
Lara Balsamello <lara.balsamello@gmail.com>
2020-02-17 10:50:01 -0300

    catch error service, 404

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -4,6 +4,7 @@ import Toolbar from "../Components/Toolbar/Toolbar";
 import SearchBox from "../Components/SearchBox/SearchBox";
 import ResultsBox from "../Components/ResultsBox/ResultsBox";
 import axios from "axios";
+import "../Layout/Layout.scss";
 import { WEATHER_API_KEY } from "../App";
 import WeatherData from "../interfaces/Weather";
 
@@ -12,7 +13,8 @@ class Layout extends Component {
     state = {
         resultsSearch: [],
         weatherSaved: [],
-        loading: false
+        loading: false,
+        error: null
     };
 
     clickHandler = (weather) => {
@@ -51,6 +53,7 @@ class Layout extends Component {
 
     catchSearch = (value) => {
         this.setState({ resultsSearch: [] })
+        this.setState({ error: null })
         if (!this.state.hasResponse) {
             this.callService(value);
         }
@@ -81,7 +84,10 @@ class Layout extends Component {
                         return 0;
                     });
                     this.setState({ resultsSearch: [...response] })
-                });
+                }).catch(Error => this.setState({
+                    error: (<div className="container-not-found"><p>No se han encontrado resultados para la búsqueda</p></div>),
+                    loading: false
+                }));
         }
 
     }
@@ -91,6 +97,7 @@ class Layout extends Component {
                 <Toolbar weatherSaved={this.state.weatherSaved} showFav={true}></Toolbar>
                 <SearchBox getSearch={this.catchSearch}></SearchBox>
                 <ResultsBox loading={this.state.loading} click={(weather) => { this.clickHandler(weather) }} results={this.state.resultsSearch}></ResultsBox>
+                {this.state.error}
             </Auxiliary>
         );
     }
